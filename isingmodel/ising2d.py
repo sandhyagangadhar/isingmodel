8f23bd8bb0a9e27a7daaf8800b327a1d9b786a4f
sandhya chintalapati <sandhya@sandhyas-MacBook-Air.local>
2021-12-23 15:12:55 +0530

    priliminary files of ising model

---
+++ isingmodel/ising2d.py
@@ -0,0 +1,18 @@
+import numpy as np
+from math import radians, cos, sin, asin, sqrt
+#array=np.ones((3,3))
+#print(array)
+size=4
+class Initial_configuration:
+    def __init__(self, size):
+        self.array=np.ones((size,size))
+
+def configuration():
+    a = Initial_configuration(size)
+    print(a.array)
+configuration()
+
+
+
+
+
---
+++ tests/test_myfunctions.py
@@ -0,0 +1,3 @@
+from isingmodel import ising2d
+def test_configuration():
+    assert ising2d.configuration()
