a9e9fb640f5564344d92218aefdbce827fc0ea23
sandhya chintalapati <sandhya@sandhyas-MacBook-Air.local>
2021-12-25 17:38:30 +0530

    done_with_mc

--- isingmodel/ising2d.py
+++ isingmodel/ising2d.py
@@ -1,7 +1,6 @@
 import numpy as np
 #import random
 from isingmodel import configuration
-#from isingmodel import energy
 
 a1=configuration.Configuration2d
 print(a1.calculate_energy())
--- isingmodel/metropolis.py
+++ isingmodel/metropolis.py
@@ -3,24 +3,20 @@ from numpy.random import rand
 from isingmodel.configuration import Configuration2d
 from isingmodel.energyutility import EnergyUtility
 
-
+list_temp = [0.2,0.5,0.8,1.0]
 class Metropolis2d:
     def apply_metropolis():
-        a2=0
-        for i in range(0,1000):
-            conf = Configuration2d()
-            energy_later, energy_before = EnergyUtility.find_energy_at_ij(conf.array, conf.a, conf.b)
-            energy_diff = (energy_later) - (energy_before)
-            if (energy_diff < 0.0):
-                conf.array[conf.a][conf.b] = conf.array[conf.a][conf.b] * -1.0
-            else:
-                a2=a2+1
-               # print(rand() * 1000)
-               # print(np.exp(-energy_diff) * 0.5)
-
-
-
-
+        for Temp in list_temp:
+            for i in range(0, 10):
+                conf = Configuration2d()
+                energy_later, energy_before = EnergyUtility.find_energy_at_ij(conf.array, conf.a, conf.b)
+                energy_diff = (energy_later) - (energy_before)
+                if (energy_diff < 0.0):
+                    conf.array[conf.a][conf.b] = conf.array[conf.a][conf.b] * -1.0
+                elif (rand() < np.exp((-1.0 * energy_diff)/Temp)):
+                    conf.array[conf.a][conf.b] = conf.array[conf.a][conf.b] * -1.0
+                else:
+                    conf.array[conf.a][conf.b] = conf.array[conf.a][conf.b]
 
 a2=Metropolis2d
 a2.apply_metropolis()
